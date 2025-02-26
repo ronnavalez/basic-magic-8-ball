5e992a63b64b993f6cee20b1b479ca66ef77ec2b
Ron Navalez <153477867+ronnavalez@users.noreply.github.com>
2025-02-26 00:12:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+let input = document.getElementById("userInput");
+let submitButton = document.getElementById("askButton");
+let response = document.getElementById("answerDisplay");
+let clear = document.getElementById("clearButton");
 
+let error = "Invalid input.";
+
+input.addEventListener("input", function(){
+    
+
+
+
+});
+
+submitButton.addEventListener("click", function(){
+    let inputBox = input.value.trim();
+    let ballResponse = [
+        "Yes.",
+        "No.",
+        "Maybe.",
+        "Try again.",
+        "Don't ask me bitch.",
+        "I think you already know the answer"
+    ];
+    let randomResponse = Math.floor(Math.random() * ballResponse.length);
+
+    if(inputBox == ""){
+        response.innerHTML = error;
+    } else{
+        response.innerHTML = ballResponse[randomResponse];
+        clear.style.display = "block";
+    }
+    
+    response.classList.add("fade-in");
+
+    setTimeout(function(){
+        response.classList.remove("fade-in");
+    }, 1000);
+})
+
+clear.addEventListener("click", function(){
+    input.value = "";
+    response.innerHTML = "";
+    clear.style.display = "none";
+})
